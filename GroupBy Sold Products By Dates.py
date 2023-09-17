04e1232a5c1b49ff50f5234970fb7216192f47fa
unknown <mehdichellak47@gmail.com>
2023-09-17 16:36:18 +0100

    some changes

--- GroupBy Sold Products By Dates.py
+++ GroupBy Sold Products By Dates.py
@@ -7,3 +7,14 @@ def categorize_products(activities: pd.DataFrame) -> pd.DataFrame:
     activities1["products"] = activities.groupby(by="sell_date")["product"].unique().agg(lambda x: ','.join(x)).reset_index(name = "products")["products"]
     
     return pd.DataFrame(activities1)
+
+
+# Good Practice 
+
+def categorize_products2(activities: pd.DataFrame) -> pd.DataFrame:
+    return activities.groupby(
+        'sell_date'
+    )['product'].agg([
+        ('num_sold', 'nunique'),
+        ('products', lambda x: ','.join(sorted(x.unique())))
+    ]).reset_index()
